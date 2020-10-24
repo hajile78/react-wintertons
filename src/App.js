74de14644149526912704aace742e59695126cc9
hajile78 <elijah@wintertons.us>
2020-10-24 09:54:40 -0400

    fix misc errors and routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom'
 
 import './App.scss';
 import Header from './partials/Header'
@@ -16,13 +16,11 @@ function App() {
             <div className="main">
               <h1 className="blog-header">Wintertons.us <small>The Whole Famn Damily</small></h1>
               <div className="postData">
-
-                  <Route exact = {true} path = "/" >
-                    <Redirect to = "/user/Main" />
-                  </Route>
+                <Switch>                  
                   <Route path = "/user/:slug" render = {(props) => (<Posts key={props.match.params.slug} slug={props.match.params.slug}/>)} />
-                  <Route path = "/post/:id" render = {(props) => (<Posts key={props.match.params.id} id={props.match.params.id} />)} />                  
-
+                  <Route path = "/post/:id" render = {(props) => (<Posts key={props.match.params.id} id={props.match.params.id} />)} />   
+                  <Redirect from = "/" to = "/user/Main" />               
+                </Switch>
               </div>
             </div>
           <LinkQuote />
--- src/partials/Posts.js
+++ src/partials/Posts.js
@@ -3,16 +3,15 @@ import ReactHtmlParser from 'react-html-parser';
 import { Link } from 'react-router-dom'
 
 function Posts(props) {
-
-	const [posts, setPosts] = useState();
-
+const [posts, setPosts] = useState();
+const { slug, id } = props;
   useEffect(() => {
 		async function getPosts() {
         const server = 'https://apiwintertons.uc.r.appspot.com';
 		//const server = 'http://localhost:5000'
 		//const server = 'http://192.168.0.14:5000'
-			const endPoint = props.slug ? `postsBy/${props.slug}` : `getPost/${props.id}`;
-			const arrName = props.slug ? `posts` : `post`;
+			const endPoint = slug ? `postsBy/${slug}` : `getPost/${id}`;
+			const arrName = slug ? `posts` : `post`;
 			console.log(`${server}/${endPoint}`)
       await fetch(`${server}/${endPoint}`).then(
         (response) => response.json()
@@ -27,25 +26,29 @@ function Posts(props) {
 			});
 	}
 	getPosts()
-	}, []);
+	}, [slug, id]);
 	
-	const showUser = (user) => props.slug === 'Main' ? '' : `by ${user}`;
-	const chunkBody = (id, body) => id ? body.substring(0, 100) : body;
+	const showUser = (user) => slug === 'Main' ? '' : `by ${user}`;
+	//const chunkBody = (id, body) => id ? body.substring(0, 100) : body;
 
 	return (	
-			<div>{posts ? posts.map((post) => {	
-				return <div id={post.id} key={post.id} >							
-					<h2>
-						<Link to={'/post/' + post.id}>{post.title}</Link>
-					</h2>
-					<p className='lead'> 
-						{showUser(post.user)}
-					</p>
-					<p><span className="glyphicon glyphicon-time"></span> {post.created}</p>				
-					<div>{ReactHtmlParser (post.body) }</div>
-					<hr />
-				</div>
-			}) : <div className="loader">Loading...</div>
+			<div>{posts ? (posts.length > 0) 
+				? posts.map((post) => {	
+					return <div id={post.id} key={post.id} >							
+						<h2>
+							<Link to={'/post/' + post.id}>{post.title}</Link>
+						</h2>
+						<p className='lead'> 
+							{showUser(post.user)}
+						</p>
+						<p><span className="glyphicon glyphicon-time"></span> {new Date(post.created).toDateString()} {new Date(post.created).toLocaleTimeString()}</p>				
+						<div class='postBody'>{ReactHtmlParser (post.body) }</div>
+						<hr />
+					</div>
+				}) 
+				: <h4>No Content Here :(</h4> 
+			: <div className="loader">Loading...</div>
+
 			}</div>
 	)
 }
